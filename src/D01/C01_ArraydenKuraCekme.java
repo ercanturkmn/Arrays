cb2b519cfbb97a59116d789989f39d2e21bb2423
ercanturkmn <turkmenercn@gmail.com>
2023-11-03 00:14:06 +0300

    arrays calisma

---
+++ src/D01/C01_ArraydenKuraCekme.java
@@ -0,0 +1,15 @@
+package D01;
+
+import java.util.Random;
+
+public class C01_ArraydenKuraCekme {
+    public static void main(String[] args) {
+        //Bir sınıf listesinden sonraki dersi anlatacak öğrencileri rastgele belirleyiniz
+
+        String[]ogrenciler={"Azim", "Ercan", "Humeyra","Ihsan","Ayca","Doruk","Burcu","Senayda"};
+        Random rnd=new Random();//rastgele olması için...
+        int dersAnlatacakKisi= rnd.nextInt(ogrenciler.length);//burada dizi içerisindeki ders anlatacak kişi indeksi belirlenir
+        String sanslıKisi=ogrenciler[dersAnlatacakKisi];//bu indexteki isim sanslıKisi ye atanır
+        System.out.println(sanslıKisi);//ve yazdırılır
+    }
+}
